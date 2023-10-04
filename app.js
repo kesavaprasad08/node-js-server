b2598239d07eb4730e320eeb786b344d41f93149
kesavaprasad08 <kesav2661998@gmail.com>
2023-10-04 12:20:59 +0530

    read file from user

--- app.js
+++ app.js
@@ -1,7 +1,34 @@
 const http= require('http');
+const fs = require('fs');
+
+
 const server = http.createServer((req, res) => {
+    let content = fs.readFileSync('message.txt','utf8');
     let url = req.url;
-    // console.log(url);
+    let method = req.method;
+
+    if(url==='/'){
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write(`<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button><p>${content}</p></form></body>`)
+        res.write('</html>');
+        return res.end();
+    }
+    if(url==='/message' && method ==='POST'){
+        const body=[];
+        req.on('data',(chunk) => {
+body.push(chunk);
+        });
+        req.on('end',() => {
+        const parseBody = Buffer.concat(body).toString();
+        const message = parseBody.split('=')[1];
+        fs.writeFileSync('message.txt',message);
+        })
+        
+        res.statusCode=302;
+        res.setHeader('Location','/');
+        return res.end();
+    }
     if(url==='/home'){
         
     res.setHeader('Content-Type','text/html');
