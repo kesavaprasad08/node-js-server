a742c29123a6afbf3f163edae64265a29b72489d
kesavaprasad08 <kesav2661998@gmail.com>
2023-10-04 12:22:27 +0530

    some changes

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const server = http.createServer((req, res) => {
     if(url==='/'){
         res.write('<html>');
         res.write('<head><title>Enter Message</title></head>');
-        res.write(`<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button><p>${content}</p></form></body>`)
+        res.write(`<body><p>${content}</p><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>`)
         res.write('</html>');
         return res.end();
     }
