4c95c94c6c0cf68e90f06fd396ebaf83385b7431
kesavaprasad08 <kesav2661998@gmail.com>
2023-10-07 20:53:24 +0530

    express

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-const http= require('http');
-const routes = require('./routes');
+const express = require("express");
 
-
-const server = http.createServer(routes);
-server.listen(3000);
+const app = express();
+app.use((req, res, next) => {
+  console.log("In the Middleware");
+  next();
+});
+app.use((req, res, next) => {
+  console.log("In the another Middleware");
+  res.send("<h1>Hello from Express!</h1>");
+});
+app.listen(3000);
