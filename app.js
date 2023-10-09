520c48768824e8b4b364e14d01aedd0f64744a56
kesavaprasad08 <kesav2661998@gmail.com>
2023-10-09 16:18:05 +0530

    getting input

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
-app.use((req, res, next) => {
-  console.log("In the Middleware");
-  next();
+
+app.use(bodyParser.urlencoded());
+
+app.use('/add-product',(req, res, next) => {
+  res.send('<form action="/product" method="POST"><input type="text" name="title"/><input type="text" name="size"/><button type="submit">Add Product</button></form>');
 });
-app.use((req, res, next) => {
-  console.log("In the another Middleware");
+
+app.use('/product',(req,res,next)=> {
+  console.log(req.body);
+  res.redirect('/');
+})
+
+app.use('/',(req, res, next) => {
   res.send("<h1>Hello from Express!</h1>");
 });
 app.listen(3000);
